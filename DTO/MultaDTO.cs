81bd280b39765fda9a172fdceced4bc188b48231
NotFabianML <80232944+NotFabianML@users.noreply.github.com>
2024-11-07 21:05:19 -0600

    add numeroPlaca y cedulaInfractor

--- DTO/MultaDTO.cs
+++ DTO/MultaDTO.cs
@@ -5,7 +5,11 @@ namespace DTO
     public class MultaDTO
     {
         public Guid IdMulta { get; set; }
-        public Guid VehiculoId { get; set; }
+
+        public string NumeroPlaca { get; set; } // Nueva propiedad para la placa
+
+        public string CedulaInfractor { get; set; } // Nueva propiedad para la cédula del infractor
+
         public Guid UsuarioIdOficial { get; set; }
         public DateTime FechaHora { get; set; }
         public decimal Latitud { get; set; }
@@ -13,7 +17,7 @@ namespace DTO
         public string? Comentario { get; set; }
         public string? FotoPlaca { get; set; }
 
-        public int Estado { get; set; } /// Representación numérica del enum EstadoMulta (1=Pendiente, 2=En Disputa, 3=Pagada)
+        public int Estado { get; set; } // Representación numérica del enum EstadoMulta (1=Pendiente, 2=En Disputa, 3=Pagada)
 
         public decimal? MontoTotal { get; set; }
         public List<InfraccionDTO> Infracciones { get; set; } = new List<InfraccionDTO>();
