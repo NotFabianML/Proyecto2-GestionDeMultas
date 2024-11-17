c2ce1e2afc4e027c764c4a4287ceb1452b6bf88a
NotFabianML <80232944+NotFabianML@users.noreply.github.com>
2024-11-17 15:52:23 -0600

    Add DTOs del emailservice

---
+++ DTO/ForgotPasswordDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class ForgotPasswordDto
+    {
+        public string Email { get; set; }
+        public string ResetUrl { get; set; } // La URL base para restablecer la contraseña
+    }
+}
