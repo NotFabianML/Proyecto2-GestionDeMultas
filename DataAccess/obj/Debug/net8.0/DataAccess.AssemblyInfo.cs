f70b89b8a45521fb26900dc767d019641f698f2b
NotFabianML <80232944+NotFabianML@users.noreply.github.com>
2024-11-18 22:31:16 -0600

    update appsettings

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79f4c73ea494a7a68fc28bdceebdf29ecc24a502")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11fcff6e7bb0edd4b975203c020b623feae43605")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
